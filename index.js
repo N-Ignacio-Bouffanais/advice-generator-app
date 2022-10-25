4fc11292dd24bcff16635456829bab76f2866141
Nicolas <nicolas.bouffanais.1999@gmail.com>
2022-10-25 20:22:36 -0300

    axios and inne.html ready for deploy, but I need to add the part of rsponsive design, especificallly the part of img display none when the screen is movile size

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const axios = require('axios');
 
-const GetData = async () => {
+const sendGetRequest = async () => {
     try {
         const resp = await axios.get('https://api.adviceslip.com/advice');
         console.log(resp.data);
+        console.log(resp.data.slip.id)
+        document.getElementById("id").innerHTML = resp.data.slip.id;
+        document.getElementById("quote").innerHTML = resp.data.slip.advice;
     } catch (err) {
         console.error(err);
     }
 };
-GetData();
+
+sendGetRequest();
